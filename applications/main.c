5180efb0c0e85c1b3455cb68339a1aad1c9b4ecb
Warfalcon-Spunky <13983089826@163.com>
2019-08-02 17:17:26 +0800

    增加虚拟串口的初始化调用

--- applications/main.c
+++ applications/main.c
@@ -12,7 +12,45 @@
 #include <rtdevice.h>
 #include <board.h>
 
+#if defined(RT_USING_POSIX)
+#include <dfs_posix.h>
+#include <dfs_poll.h>
+#include <libc.h>
+static int dev_old_flag;
+#endif
+
+#define LOG_TAG                        "app.main"
+#include <at_log.h>
+
 int main(void)
 {
     return RT_EOK;
 }
+
+int vcom_init(void)
+{
+    /* set console */
+    rt_console_set_device("vcom");
+    
+#if defined(RT_USING_POSIX)
+    /* backup flag */
+    dev_old_flag = ioctl(libc_stdio_get_console(), F_GETFL, (void *) RT_NULL);
+    /* add non-block flag */
+    ioctl(libc_stdio_get_console(), F_SETFL, (void *) (dev_old_flag | O_NONBLOCK));
+    /* set tcp shell device for console */
+    libc_stdio_set_console("vcom", O_RDWR);
+    /* resume finsh thread, make sure it will unblock from last device receive */
+    rt_thread_t tid = rt_thread_find(FINSH_THREAD_NAME);
+    if (tid)
+    {
+        rt_thread_resume(tid);
+        rt_schedule();
+    }
+#else
+    /* set finsh device */
+    finsh_set_device("vcom");
+#endif /* RT_USING_POSIX */
+    
+    return 0;
+}
+INIT_ENV_EXPORT(vcom_init);
